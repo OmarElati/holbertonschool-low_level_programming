a88ec91144b5c8a480b8548c529d4a69b4c8e982
OmarElati <5796@holbertonstudents.com>
2022-10-04 12:37:40 +0100

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 printf("%s %d is ", last, n);
 if (n % 10 > 5)
 {
-printf("%d and is greater than 5\n ", n % 10);
+printf("%d and is greater than 5\n", n % 10);
 }
 else if (n % 10 == 0)
 {
-printf("%d and is 0\n ", n % 10);
+printf("%d and is 0\n", n % 10);
 }
 else
 {
-printf("%d and is less than 6 and not 0\n ", n % 10);
+printf("%d and is less than 6 and not 0\n", n % 10);
 }
 return (0);
 }
