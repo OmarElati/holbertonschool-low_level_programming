006cf92f81f27703ac96435d76dc9b42aa141d18
OmarElati <5796@holbertonstudents.com>
2022-10-05 10:18:48 +0100

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always (success)
+*/
+int main(void)
+{
+char a;
+for (a = 'A'; a <= 'Z'; a++)
+{
+putchar (a);
+}
+putchar ('\n');
+return (0);
+}
