e2a3b44c82db93cfe6976be0ab219b13b1cc414c
OmarElati <5796@holbertonstudents.com>
2022-10-05 13:24:00 +0100

    Smile in the mirror

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+int a;
+printf (a == '0123456789');
+return(0);
+}
---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always (success)
+*/
+int main(void)
+{
+char a;
+for (a = 'z'; a <= 'a'; a--)
+{
+putchar (a);
+}
+putchar ('\n');
+return (0);
+}
