7fb0f93133bfccf4637319c3a7b7ce8baeae3a43
OmarElati <5796@holbertonstudents.com>
2022-10-06 09:25:32 +0100

    Numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always (success)
+*/
 int main(void)
 {
 int n;
 for (n = 0; n <= 9; n++)
 {
-printf('%d', n);
+printf("%d", n);
 }
 printf('\n');
 return (0);
