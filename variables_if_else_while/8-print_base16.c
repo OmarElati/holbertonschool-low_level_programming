bd7d80e6825d8fafa526aebcd0764e5f9bf11648
OmarElati <5796@holbertonstudents.com>
2022-10-06 10:11:07 +0100

    Hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 char n;
-for (n = '0' ; n <= '9'; n++)
+for (n = '0'; n <= '9'; n++)
 {
 putchar(n);
 }
-for (n = 'a' ; n <= 'f'; n++)
+for (n = 'a'; n <= 'f'; n++)
 {
 putchar(n);
 }
