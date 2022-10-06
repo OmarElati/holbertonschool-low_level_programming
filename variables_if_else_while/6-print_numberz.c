9a27401129a16845584e2e2113291e60f0c1d6f6
OmarElati <5796@holbertonstudents.com>
2022-10-06 09:34:57 +0100

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always (success)
+*/
+int main(void)
+{
+    int n
+    for (n = '0'; n <= '9'; n++)
+    {
+        putchar(0);
+    }
+    putchar("\n");
+    return (0);
+}    
