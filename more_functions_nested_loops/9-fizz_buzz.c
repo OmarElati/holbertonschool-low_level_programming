fe3d6ecdf6b678e75e4b6b7f5c828f3d1042167d
OmarElati <5796@holbertonstudents.com>
2022-10-07 23:04:36 +0100

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints a Fizz program
+*
+* Return: 0
+*/
+
+int main(void)
+{
+int a;
+for (a = 0; a <= 100; a++)
+{
+if ((a % 3 == 0) && (a % 5 == 0))
+{
+printf("FizzBuzz");
+}
+else if (a % 5 == 0)
+{
+printf("Fizz");
+}
+else
+{
+printf("%d", a);
+}
+if (a != 100)
+{
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
