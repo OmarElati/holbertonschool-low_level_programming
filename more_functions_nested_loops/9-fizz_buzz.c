2ee4e0c16eb590a808f2c16eb202ee24ec117f23
OmarElati <5796@holbertonstudents.com>
2022-10-07 23:08:54 +0100

    Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,13 +10,13 @@
 int main(void)
 {
 int a;
-for (a = 0; a <= 100; a++)
+for (a = 1; a <= 100; a++)
 {
 if ((a % 3 == 0) && (a % 5 == 0))
 {
 printf("FizzBuzz");
 }
-else if (a % 5 == 0)
+else if (a % 3 == 0)
 {
 printf("Fizz");
 }
