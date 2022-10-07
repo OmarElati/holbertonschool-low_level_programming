68714e164691a9f92b5673078107c869b2e78905
OmarElati <5796@holbertonstudents.com>
2022-10-07 11:33:59 +0100

    10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int f;
+char c;
+for (f = 1; f <= 9; f++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar (c);
+}
+_putchar ('\n');
+}
+}
