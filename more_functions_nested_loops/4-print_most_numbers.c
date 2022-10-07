3bcfe775c460dd22888cc8a9dcd2f6ef47e5324a
OmarElati <5796@holbertonstudents.com>
2022-10-07 22:08:21 +0100

    I believe in numbers and signs

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* print_numbers - prints the numbers from 0 to 9
+* print_most_numbers - prints the numbers from 0 to 9 without 2 and 4
 *
 * Return: 0
 */
 
-void print_numbers(void)
+void print_most_numbers(void);
 {
 int n;
 for (n = 0; n <= 9; n++)
