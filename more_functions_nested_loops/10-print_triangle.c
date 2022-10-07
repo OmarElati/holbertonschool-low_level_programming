bb5484ba99cc0f94e7fa8f2911e01208abb6734b
OmarElati <5796@holbertonstudents.com>
2022-10-07 23:15:29 +0100

    Triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_triangle - prints a triangle
+* @size: int
+* Return: Empty
+*/
+
+void print_triangle(int size)
+{
+int a, b, c;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++)
+{
+_putchar(32);
+}
+for (c = 0; c < size; c++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
