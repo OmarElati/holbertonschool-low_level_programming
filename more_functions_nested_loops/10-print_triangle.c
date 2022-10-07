a5844044794b594828a35e33b260861ae507606e
OmarElati <5796@holbertonstudents.com>
2022-10-07 23:18:27 +0100

    Triangles

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -17,11 +17,11 @@ else
 {
 for (a = 0; a < size; a++)
 {
-for (b = 0; b < size; b++)
+for (b = size - a; b > 1; b--)
 {
 _putchar(32);
 }
-for (c = 0; c < size; c++)
+for (c = 0; c <= a; c++)
 {
 _putchar(35);
 }
