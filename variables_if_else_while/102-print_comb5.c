db29da0a7b95c2735e94d202909a83e69152f669
OmarElati <5796@holbertonstudents.com>
2022-10-09 22:11:09 +0100

    Software is eating the World

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+*Return: o
+*/
+int main(void)
+{
+int a, b, c, d;
+for (a = '0'; a <= '9'; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
+for (c = '0'; c <= '9'; c++)
+{
+for (d = '1'; d <= '9'; d++)
+{
+if ((c + d) > (a + b))
+{
+putchar(a);
+putchar(b);
+putchar(32);
+putchar(c);
+putchar(d);
+if ((c = d = '9') && (a+b != 98))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
