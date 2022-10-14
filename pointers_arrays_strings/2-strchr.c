6dfd823933311e8faaa1c88933b30356237681a3
OmarElati <5796@holbertonstudents.com>
2022-10-14 20:14:08 +0100

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ *
+ * @dest: destination of copy
+ * @s: string
+ * @c: character 
+ *
+ * Return: s or NULL
+*/
+
+char *_strchr(char *s, char c)
+{
+int a;
+for (a = 0; s[a]; a++)
+{
+if (s[a] == c)
+{
+return (s + a);
+}
+}
+if (s[a] == c)
+{
+return (s + a);
+}
+return (0);
+}
