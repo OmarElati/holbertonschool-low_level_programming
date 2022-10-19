89f615ab0c4c815c9172476db167f097c8cf400c
OmarElati <5796@holbertonstudents.com>
2022-10-19 17:42:49 +0100

    You mustn't be afraid to dream a little bigger, darling

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -10,18 +10,19 @@
 
 int factorial(int n)
 {
-	int a;
+	int a = n;
+
 	if (n < 0)
 	{
 		return (-1);
 	}
-	else if ((n = 0) || (n = 1))
+	else if ((n == 0) || (n == 1))
 	{
 		return (1);
 	}
 	else
 	{
-	    a *= factorial(n);
+		a *= factorial(n - 1);
 	}
-	return (a); 
+	return (a);
 }
