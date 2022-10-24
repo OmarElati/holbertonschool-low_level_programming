701a25ce50d72f4dc944989d8d6ffec9be387887
OmarElati <5796@holbertonstudents.com>
2022-10-24 21:53:13 +0100

    If you even dream of beating me you'd better wake up and apologize

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-    c = (int **)malloc(sizeof(int *) * height);
+    c[a] = (int **)malloc(sizeof(int *) * height);
 	if (c[a] == NULL)
 	{
 		return (NULL);
