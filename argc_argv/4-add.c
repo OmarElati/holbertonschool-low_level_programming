6d25c899bd645601436f7259149d517c43a586f5
OmarElati <5796@holbertonstudents.com>
2022-10-24 20:11:38 +0100

    To infinity and beyond

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,19 +13,19 @@ int main(int argc, char *argv[])
 
 	for (m = 1; m < argc; m++)
 	{
-        for (y = 0; argv[m][y]; y++)
-        {
-            if (isdigit(argv[m][y]) == 0)
-            {
-                puts("Error");
-                return (1);
-            }
-        }
+		for (y = 0; argv[m][y]; y++)
+		{
+			if (isdigit(argv[m][y]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
+		}
 	}
 	for (m = 1; m < argc; m++)
 	{
 		n += atoi(argv[m]);
 	}
-    printf("%d\n", n);
+	printf("%d\n", n);
 	return (0);
 }
