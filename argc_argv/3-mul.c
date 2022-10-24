71e81b469e0b386a8495fe39c0ce743c32edcd90
OmarElati <5796@holbertonstudents.com>
2022-10-24 19:47:37 +0100

    Neither irony nor sarcasm is argument

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - prints multiplies two numbers
  * @argc: Number of command line
@@ -7,9 +8,13 @@
  */
 int main(int argc, char *argv[])
 {
+    int n, m, y;
 	if (argc == 3)
     {
-        printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+        n = atoi(argv[1]);
+        m = atoi(argv[2]);
+        y = n * m;
+        printf("%d\n", y);
     }
     else
 	{
