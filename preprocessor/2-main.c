0ca411d26c071a8609cb4eba4d7e6fc54edceb43
OmarElati <5796@holbertonstudents.com>
2022-10-27 06:58:53 +0100

    program that prints the name of the file

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include "pi.h"
+#include "1-pi.h"
 
 /**
  * main -check the code
- * 
+ *
  * Return: Always 0
-*/
+ */
 
 int main(void)
 {
-printf("%s\n", HEADER_FILE);
-return (0);
+	printf("%s\n", HEADER_FILE);
+	return (0);
 }
