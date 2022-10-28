f1dad69f96ab09052d1ec5fcf105d36444b592a9
OmarElati <5796@holbertonstudents.com>
2022-10-28 21:49:48 +0100

    reallocates a memory block added

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -5,42 +5,42 @@
  * _realloc - function that reallocates a memory block
  *
  * @ptr: pointer to the memory previously allocated
- * @old_size: the size of the allocated space 
+ * @old_size: the size of the allocated space
  * @new_size: the new size of the new memory block
  *
  * Return: pointer
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    char *tptr;
-    unsigned int i;
+	char *tptr;
+	unsigned int i;
 
-    if (new_size == old_size)
-    {
-        return (ptr);
-    }
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
 
-    if (ptr != NULL && new_size ==0 )
-    {
-        free(ptr);
-        return (NULL);
-    }
-    
-    if (ptr == NULL)
-    {
-        ptr = malloc(new_size);
-        return (ptr);
-    }
-    tptr = malloc(new_size);
-    if (tptr == NULL)
-    {
-        return(NULL);
-    }
-    for (i = 0; i < old_size && i < new_size; i++)
-    {
-        tptr[i] = ((char *)ptr)[i];
-    }
-    
-    free(ptr);
-    return (tptr);
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	tptr = malloc(new_size);
+	if (tptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		tptr[i] = ((char *)ptr)[i];
+	}
+
+	free(ptr);
+	return (tptr);
 }
