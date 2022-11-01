711e8b66d0d85ede2a426bf7fae8b61dffb9f989
OmarElati <5796@holbertonstudents.com>
2022-11-01 15:10:54 +0100

    Stract added

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,17 @@
-#include <stdio.h>
-struct dog
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - ..
+* @name: ..
+* @age: ..
+* @owner: ..
+*/
+typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+#endif
