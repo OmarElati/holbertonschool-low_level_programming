ae2983709a41e7d1f324c6d7f93500025e0ba33c
OmarElati <5796@holbertonstudents.com>
2022-11-03 13:16:48 +0100

    print struct added

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -20,23 +20,16 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: (nil)\n");
 		}
-		if (d->name != NULL)
+		else
 		{
 			printf("Name: %s\n", d->name);
-		}
-		if ((d->age) == 0.0)
-		{
-			printf("Age: %f\n", d->age);
-		}
-		if (d->age != 0)
-		{
 			printf("Age: %f\n", d->age);
 		}
 		if (d->owner == NULL)
 		{
 			printf("Owner: (nil)\n");
 		}
-		if (d->owner != NULL)
+		else
 		{
 			printf("Owner: %s\n", d->owner);
 		}
