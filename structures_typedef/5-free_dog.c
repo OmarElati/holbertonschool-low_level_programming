7aa08a278925e216f37b0b910dd919b1b4315c73
OmarElati <5796@holbertonstudents.com>
2022-11-04 20:07:27 +0100

    frees dog added

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 
 /**
- * free_dog -
- * 
- * 
- * 
- * 
+ * free_dog - frees dogs function
+ *
+ * @d: pointer to struct
+ *
+ * Return: Empty
 */
 void free_dog(dog_t *d)
 {
-    if (d != NULL)
-    {
-        free(d);
-    }
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
