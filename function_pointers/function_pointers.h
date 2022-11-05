4cd712a61b8d0d044bc9da4c1fb3de6db19fc548
OmarElati <5796@holbertonstudents.com>
2022-11-05 12:06:37 +0100

    added

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef FUNCTION_POINTER_H
 #define FUNCTION_POINTER_H
 
+#include <stddef.h>
+
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
