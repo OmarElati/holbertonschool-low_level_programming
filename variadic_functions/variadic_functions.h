caec2cff1cbae6c4d1b9be0502aae55745c05ccf
OmarElati <5796@holbertonstudents.com>
2022-11-05 22:33:13 +0100

    added

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -6,8 +6,24 @@
 #include <stdio.h>
 #include <string.h>
 
+int _putchar(char c);
+
+/**
+* struct op - struct op
+*
+* @op: ..
+* @f: ..
+*/
+
+typedef struct op
+{
+		char *op;
+		void (*f)(va_list);
+} op_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
