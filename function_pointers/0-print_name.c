a58db32b79490a577853a3c8a0d5982a086c52f0
OmarElati <5796@holbertonstudents.com>
2022-11-05 11:04:00 +0100

    function print name added

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - function that print name 
+ *
+ * @name: name inputted
+ * @f: function pointer
+ *
+ * Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+    if (f != NULL)
+    {
+        f(name);
+    }
+}
