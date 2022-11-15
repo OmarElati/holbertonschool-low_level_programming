10e3acab6c26229a222202bb2d09628f0b05397d
OmarElati <5796@holbertonstudents.com>
2022-11-15 23:03:42 +0100

    Print list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,19 +8,19 @@
 
 size_t print_list(const list_t *h)
 {
-    int i;
+	int i;
 
-    for (i =0; h != NULL; i++)
-    {
-        if (h->str == NULL)
-        {
-            printf("[0] nil\n");
-        }
-        else
-        {
-            printf("[%i] %s\n", h->len, h->str);
-        }
-        h = h->next;
-    }
-    return (i);
+	for (i =0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] nil\n");
+		}
+		else
+		{
+			printf("[%i] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	return (i);
 }
