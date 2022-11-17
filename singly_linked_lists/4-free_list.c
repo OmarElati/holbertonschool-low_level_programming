603ef81e55aecbd8030b5046f2496877f207596f
OmarElati <5796@holbertonstudents.com>
2022-11-17 16:01:14 +0100

    Free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - Function that frees a list
+ * @head: poiter of link of list
+ * Return: Empty
+*/
+void free_list(list_t *head)
+{
+    list_t *node = head;
+    list_t *i;
+
+    while (node != NULL)
+    {
+        i = node;
+        node = node->next;
+        free(i->str);
+        free(i);
+        head = NULL;
+    }
+}
