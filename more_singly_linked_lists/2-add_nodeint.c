75fb616e41f485ddaf0dd5f8bcd39348a1dd60ef
OmarElati <5796@holbertonstudents.com>
2022-11-23 21:48:03 +0100

    Add node

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -8,15 +8,15 @@
 */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *new;
+	listint_t *new;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL && n != 0)
-    {
-        return (NULL);
-    }
-    new->n = n;
-    new->next = *head;
-    *head = new;
-    return (new);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL && n != 0)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
