d64445a08474576f8c6b3c05e266ed3308147ebe
OmarElati <5796@holbertonstudents.com>
2022-11-25 15:03:27 +0100

    creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,21 +14,23 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
-		for (len = 0; text_content[len] != '\0'; len++)
-			;
-		count = write(fd, text_content, len);
+		close(fd);
+		return (1);
 	}
-	if (count == -1)
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+	count = write(fd, text_content, len);
+	if (count < 1)
 	{
 		return (-1);
 	}
 	close(fd);
-	return (-1);
+	return (1);
 }
