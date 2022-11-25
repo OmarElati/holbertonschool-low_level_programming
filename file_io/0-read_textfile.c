c5aac0dab02a95f1eecf8b0a4a7f774321e6fe7d
OmarElati <5796@holbertonstudents.com>
2022-11-25 14:35:36 +0100

    read textfile

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,54 +1,51 @@
 #include "main.h"
 
 /**
- * read_textfile - Function that reads a text file and prints it to standard output
+ * read_textfile - Function that reads text file & prints it to output
  * @filename: the name of file to read and print
  * @letters: the number of letter in filename
  * Return: the actual number of letters it could read and print OR 0
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd = 0;
-    char *tmp;
-    ssize_t error, len = 0;
+	int fd = 0;
+	char *tmp;
+	ssize_t error, len = 0;
 
-    if (filename == NULL)
-    {
-        return (0);
-    }
-    tmp = malloc(sizeof(char) * letters);
-    if (tmp == NULL)
-    {
-        return (0);
-    }
-    fd = open (filename, O_RDONLY);
-    if (fd == -1)
-    {
-        free(tmp);
-        return(0);
-    }
-    len = read(fd, tmp, letters);
-    if (len == -1)
-    {
-        free(tmp);
-        return (0);
-    }
-    if (len > 0)
-    {
-        error = write (STDOUT_FILENO, tmp, len);
-    }
-    if (error < len)
-    {
-        free(tmp);
-        return (0);
-    }
-    error = close(fd);
-    if (error == -1)
-    {
-        free(tmp);
-        return (0);
-    }
-
-    free(tmp);
-    return (len);
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	tmp = malloc(sizeof(char) * letters);
+	if (tmp == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(tmp);
+		return (0);
+	}
+	len = read(fd, tmp, letters);
+	if (len == -1)
+	{
+		free(tmp);
+		return (0);
+	}
+	if (len > 0)
+		error = write(STDOUT_FILENO, tmp, len);
+	if (error < len)
+	{
+		free(tmp);
+		return (0);
+	}
+	error = close(fd);
+	if (error == -1)
+	{
+		free(tmp);
+		return (0);
+	}
+	free(tmp);
+	return (len);
 }
