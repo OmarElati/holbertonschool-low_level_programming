ad0088f699fae2e35ce42c0318933aeb536bee54
OmarElati <5796@holbertonstudents.com>
2022-11-25 17:14:43 +0100

    Free list

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -7,12 +7,12 @@
 */
 void free_listint(listint_t *head)
 {
-    listint_t *tmp;
+	listint_t *tmp;
 
-    while (head != NULL)
-    {
-        tmp = head;
-        head = head->next;
-        free(tmp);
-    }
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
 }
