cc12c9dd9bd377c66d6ae933003c4db786889584
OmarElati <5796@holbertonstudents.com>
2022-11-26 22:34:51 +0100

    Insert

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - Function that inserts a new node at a given position
+ * insert_nodeint_at_index - Function that inserts new node position
  * @head: pointer of pointer of head
  * @idx: index of the list
  * @n: int
@@ -9,37 +9,37 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *new;
-    unsigned int x;
+	listint_t *new;
+	unsigned int x;
 
-    new = malloc(sizeof(listint_t));
-    new->n = n;
-    if (new != NULL)
-    {
-        return (NULL);
-    }
-    if (*head == NULL)
-    {
-        *head = new;
-        new->next = NULL;
-        return (new); 
-    }
-    if (idx == 0)
-    {
-        new->next = *head;
-        *head = new;
-        return (new);
-    }
-    while (x < (idx - 1) && (*head)->next)
-    {
-        *head = (*head)->next;
-        x++;
-    }
-    if (x < idx - 1)
-    {
-        return (NULL);
-    }
-    new->next = (*head)->next;
-    (*head)->next = new;
-    return (new);
+	new = malloc(sizeof(listint_t));
+	if (new != NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+		return (new);
+	}
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	while (x < (idx - 1) && (*head)->next)
+	{
+		*head = (*head)->next;
+		x++;
+	}
+	if (x < idx - 1)
+	{
+		return (NULL);
+	}
+	new->next = (*head)->next;
+	(*head)->next = new;
+	return (new);
 }
