e28630fc874dffa9a3c77758ede7a73917dd3f8a
OmarElati <5796@holbertonstudents.com>
2022-12-01 22:41:48 +0100

    Insert

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -17,11 +17,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	new->n = n;
 	if (head == NULL)
 	{
 		return (new);
 	}
+	new->n = n;
 	if (idx == 0)
 	{
 		new->next = *head;
@@ -35,6 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		x++;
 		if (tmp == NULL)
 		{
+			free(new);
 			return (NULL);
 		}
 	}
