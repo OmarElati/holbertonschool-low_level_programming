78aa80ff18627c89e4baac6df6a6a110f4d74f2e
OmarElati <5796@holbertonstudents.com>
2022-12-04 13:50:13 +0100

    Free list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - function that frees a list
+ * @head: pointer of list
+ * Return: Empty
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp_node;
+
+	while (head != NULL)
+	{
+		tmp_node = head;
+		tmp_node = tmp_node->next;
+		free(tmp_node);
+	}
+}
