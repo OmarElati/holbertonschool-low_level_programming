f0b46b1e41192f6128e26aa952b325dba86a9f2d
OmarElati <5796@holbertonstudents.com>
2022-12-04 14:10:26 +0100

    Get node at index

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -8,20 +8,20 @@
 */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
-    dlistint_t *tmp_node = head;
+	unsigned int i = 0;
+	dlistint_t *tmp_node = head;
 
-    while (i < index && tmp_node != NULL)
-    {
-        tmp_node = tmp_node->next;
-        i++;
-    }
-    if (i == index)
+	while (i < index && tmp_node != NULL)
+	{
+		tmp_node = tmp_node->next;
+		i++;
+	}
+	if (i == index)
 	{
 		return (tmp_node);
 	}
 	else
 	{
-	    return (NULL);
-    }
+		return (NULL);
+	}
 }
