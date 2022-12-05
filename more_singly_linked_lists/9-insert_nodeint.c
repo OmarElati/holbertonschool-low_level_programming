77fb1d0c6408cfaa84f741324f68f136b72045bc
OmarElati <5796@holbertonstudents.com>
2022-12-05 21:07:14 +0100

    Insert

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	*tmp_node = *head;
+	tmp_node = *head;
 	while (x < (idx - 1) && tmp_node->next)
 	{
 		tmp_node = tmp_node->next;
