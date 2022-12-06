a676f48351721c4eb3122198fe530599c19bc890
OmarElati <5796@holbertonstudents.com>
2022-12-06 18:59:09 +0100

    Insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *tmp_node;
-	unsigned int x;
+	unsigned int x = 0;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
