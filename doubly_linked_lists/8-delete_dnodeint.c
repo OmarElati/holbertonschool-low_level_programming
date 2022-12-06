a0098a5b7e011b8770be062e25d5933951384387
OmarElati <5796@holbertonstudents.com>
2022-12-06 19:12:12 +0100

    Delete at index

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -5,7 +5,7 @@
  * @index: index of linked list
  * Return: 1 if it succeeded, -1 if it failed
 */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
     dlistint_t *d_node, *tmp_node;
 	unsigned int x = 0;
