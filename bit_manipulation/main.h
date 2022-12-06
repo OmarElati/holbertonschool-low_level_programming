745f9fe9a70a33cc3b6399d98532ffc12328369a
OmarElati <5796@holbertonstudents.com>
2022-12-06 12:34:23 +0100

    HEADER file

---
+++ bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned int binary_to_uint(const char *b);
+unsigned int _pow(unsigned int x, unsigned int y);
+
+#endif /* MAIN_H */
