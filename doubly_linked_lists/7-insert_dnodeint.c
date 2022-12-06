486697158c190ebc1b00eeafbe01a64ee97a0110
OmarElati <5796@holbertonstudents.com>
2022-12-06 11:19:40 +0100

    Insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -31,14 +31,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 	tmp_node = *h;
-	while (x < (idx - 1) && tmp_node->next)
+	while (x < (idx - 1) && tmp_node->next != NULL)
 	{
 		tmp_node = tmp_node->next;
 		x++;
 	}
-	if (tmp_node == NULL)
+	if (x < idx - 1)
 	{
-		free(new_node);
 		return (NULL);
 	}
 	new_node->next = tmp_node->next;
