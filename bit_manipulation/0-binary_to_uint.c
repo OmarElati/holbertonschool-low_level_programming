46de78e8d1403d98cc7e6faca5e30b0183366ec8
OmarElati <5796@holbertonstudents.com>
2022-12-06 13:38:36 +0100

    binary to un_int

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -26,7 +26,7 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 	while (b[len] != '\0')
-			len++;
+		len++;
 	for (i = len - 1; i >= 0; i--)
 	{
 		if (b[i] != '1' && b[i] != '0')
