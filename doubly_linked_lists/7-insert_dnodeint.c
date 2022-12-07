577464560aad272b757580dcba060b6e9c2d2471
OmarElati <5796@holbertonstudents.com>
2022-12-07 14:10:51 +0100

    Insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,37 +10,32 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *tmp_node;
-	unsigned int x = 0;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	while (*h)
 	{
-		return (NULL);
-	}
-	new_node->n = n;
-	if (*h == NULL)
-	{
-		*h = new_node;
-		new_node->next = NULL;
+		new_node = malloc(sizeof(dlistint_t));
+		tmp_node = *h;
+		new_node->n;
+
+		if (idx == 0)
+		{
+			new_node->next = *h;
+			*h = new_node;
+			return (new_node);
+		}
+		while (0 < (idx - 1) && tmp_node->next != NULL)
+		{
+			tmp_node = tmp_node->next;
+			idx--;
+		}
+		if (x < idx - 1)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->next = tmp_node->next;
+		tmp_node->next = new_node;
 		return (new_node);
 	}
-	if (idx == 0)
-	{
-		new_node->next = *h;
-		*h = new_node;
-		return (new_node);
-	}
-	tmp_node = *h;
-	while (x < (idx - 1) && tmp_node->next != NULL)
-	{
-		tmp_node = tmp_node->next;
-		x++;
-	}
-	if (x < idx - 1)
-	{
-		return (NULL);
-	}
-	new_node->next = tmp_node->next;
-	tmp_node->next = new_node;
-	return (new_node);
+	return (NULL);
 }
