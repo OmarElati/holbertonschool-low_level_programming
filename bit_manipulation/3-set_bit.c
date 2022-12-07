ae1a0a527ad8dc086e984e24310b150f7ec7addc
OmarElati <5796@holbertonstudents.com>
2022-12-07 10:17:51 +0100

    set bit

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * ste_bit - set 1 bit by index 
+ * @n: adress of integer
+ * @index: unsigned integer
+ * Return: 1 OR -1
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(*n) * 8))
+		return (-1);
+	*n |= 1 << index;
+	return (1);
+}
