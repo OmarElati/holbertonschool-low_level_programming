fb681e2b57c9b8bca89b99c4350f31629d6fa0db
OmarElati <5796@holbertonstudents.com>
2022-12-07 14:22:50 +0100

    Insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,31 +10,37 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *tmp_node;
+	unsigned int x = 0;
 
-	while (*h)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 	{
-		new_node = malloc(sizeof(dlistint_t));
-		tmp_node = *h;
-		new_node->n = n;
-
-		if (idx == 0)
-		{
-			new_node->next = NULL;
-			return (new_node);
-		}
-		while (0 < (idx - 1) && tmp_node->next != NULL)
-		{
-			tmp_node = tmp_node->next;
-			idx--;
-		}
-		if (tmp_node->next == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		new_node->next = tmp_node->next;
-		tmp_node->next = new_node;
+		return (NULL);
+	}
+	new_node->n = n;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->next = NULL;
 		return (new_node);
 	}
-	return (NULL);
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	tmp_node = *head;
+	while (x < (idx - 1) && tmp_node->next != NULL)
+	{
+		tmp_node = tmp_node->next;
+		x++;
+	}
+	if (x < idx - 1)
+	{
+		return (NULL);
+	}
+	new_node->next = tmp_node->next;
+	tmp_node->next = new_node;
+	return (new_node);
 }
