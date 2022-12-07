eed3b846cb5eacfa90e1e1a51575bd75117bc06e
OmarElati <5796@holbertonstudents.com>
2022-12-07 10:06:56 +0100

    get bit

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_bit - get bit 
+ * @n: integer
+ * @index: integer
+ * Return: integer
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+    int i;
+
+    i = ((n >> index) & 1);
+    if (index > 100)
+        return (-1);
+    return (i);
+}
