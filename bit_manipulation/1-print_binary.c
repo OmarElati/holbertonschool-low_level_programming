824b63a4d6e0d3123b95562ec99741a1c55a059b
OmarElati <5796@holbertonstudents.com>
2022-12-07 09:58:16 +0100

    convertir from base 10 -> 2

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_binary - From base 10 to base 2
+ * @n: unsigned long integer
+ * Return: Empty
+*/
+void print_binary(unsigned long int n)
+{
+    if (n > 1)
+        print_binary(n >> 1);
+    putchar((n & 1) + '0');
+}
