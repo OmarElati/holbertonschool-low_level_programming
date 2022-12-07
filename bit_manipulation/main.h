66fc904e9a1a5066d93b48f9a9a73ec3ed18498d
OmarElati <5796@holbertonstudents.com>
2022-12-07 10:21:31 +0100

    HEADER file

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -10,5 +10,6 @@ unsigned int _pow(unsigned int x, unsigned int y);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif /* MAIN_H */
