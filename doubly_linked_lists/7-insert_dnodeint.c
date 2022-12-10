c95843794f4b3f52ce8534204ecd28065dd1652f
OmarElati <5796@holbertonstudents.com>
2022-12-10 19:43:57 +0100

    Insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,30 +17,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	new_node->n = n;
-	if (*h == NULL)
-	{
-		*h = new_node;
-		new_node->next = NULL;
-		return (new_node);
-	}
+	tmp_node->n = n;
+	new_node = *h;
 	if (idx == 0)
 	{
-		new_node->next = *h;
-		*h = new_node;
-		return (new_node);
+		if (*h)
+		{
+			tmp_node->next = *h;
+			(*h)->prev = tmp_node;
+		}
+		*h = tmp_node;
+		return (*h);
 	}
-	tmp_node = *h;
-	while (x < (idx - 1) && tmp_node->next != NULL)
+	while (x < (idx - 1) && tmp_node)
 	{
-		tmp_node = tmp_node->next;
+		new_node = new_node->next;
 		x++;
 	}
-	if (x < idx - 1)
+	if (new_node)
 	{
-		return (NULL);
+		if (new_node->next)
+		{
+			new_node->next->prev = tmp_node;
+		}
+		tmp_node->next = new_node->next;
+		tmp_node->prev = new_node;
+		new_node->next = tmp_node;
+		return (tmp_node);
 	}
-	new_node->next = tmp_node->next;
-	tmp_node->next = new_node;
-	return (new_node);
+	return (NULL);
 }
