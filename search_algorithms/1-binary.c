839ea4d2e653fa77f115cddf9efa43245a83ea76
OmarElati <5796@holbertonstudents.com>
2023-01-06 12:21:16 +0100

    Binary search

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -5,16 +5,38 @@
  * @array: is a pointer to the first element of the array to search in
  * @size: is the number of elements in array
  * @value: is the value to search for
- * 
+ *
  * Return: the index where value is located OR -1
 */
 int binary_search(int *array, size_t size, int value)
 {
-	value = -1;
+	size_t x, Upper, Lower;
 
-	if (!array || size <= 0)
+	if (array == NULL)
 	{
-		return (value);
+		return (-1);
 	}
-	return (value);
+	for (Upper = 0, Lower = size - 1; Lower >= Upper;)
+	{
+		printf("Search in array: ");
+		for (x = Upper; x < Lower; x++)
+		{
+			printf("%d", array[x]);
+		}
+		printf("%d\n", array[x]);
+		x = Upper + (Lower - Upper) / 2;
+		if (array[x] == value)
+		{
+			return (x);
+	    }
+		if (array[x] > value)
+		{
+			Lower = x - 1;
+		}
+		else
+		{
+			Upper = x + 1;
+		}
+	}
+	return (-1);
 }
