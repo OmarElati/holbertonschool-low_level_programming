4ba00018fd91c36628ca6b0090bc52f6861c37c9
OmarElati <5796@holbertonstudents.com>
2023-01-06 11:21:16 +0100

    HEADER file

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,6 +1,10 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /* prototypes */
 int linear_search(int *array, size_t size, int value);
 
